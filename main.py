f5c8d32ee4d4636859fab3c8cce87acac17392fa
open-source-wize <badrmohamedashraf@outlook.com>
2025-01-02 22:44:55 +0200

    add tests

--- main.py
+++ main.py
@@ -8,4 +8,16 @@ def multiply(x,y):
     return x * y
 
 def divide(x,y):
-    return x /y
+    return x / y
+
+def test_divide():
+    assert divide(6,2) == 3
+
+def test_add():
+    assert add(1,2) == 3
+
+def test_multiply():
+    assert multiply(2,3) == 6
+
+def test_sub():
+    assert sub(6,2) == 4
